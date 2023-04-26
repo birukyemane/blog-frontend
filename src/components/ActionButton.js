4b8bfefa1f93dbe28b94c9ddfc09bc3c20248ab9
birukyemane <bir_yem@yahoo.com>
2023-04-26 03:16:33 -0500

    - refactor ActionButton
     - add name props to render different button names and icon

--- src/components/ActionButton.js
+++ src/components/ActionButton.js
@@ -1,12 +1,11 @@
 import { Form } from "react-router-dom";
-import DeleteIcon from "@mui/icons-material/Delete";
 
 const ActionButton = ({ urlsegment, name }) => {
     console.log('action button', urlsegment)
   return (
     <Form method="post" action={urlsegment}>
       <button style={{borderStyle: "none"}} variant="text">
-        <DeleteIcon />
+        {name}
       </button>
       
     </Form>
